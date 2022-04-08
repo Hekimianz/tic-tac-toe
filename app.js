e677065039434ddfd92c621e9ac7b0222385b1fb
Aram Hekimian <aramhek121@gmail.com>
2022-04-07 23:55:28 -0500

    Work in progress

--- app.js
+++ app.js
@@ -26,62 +26,76 @@ const board = (() => {
     return { Gameboard, gameCont };
 })();
 
-const Player = (name, mark) => {
-    const sayHello = () => console.log(`Hello, ${name}. Your`)
-    const playerMark = mark
-    return { name, sayHello, playerMark };
-}
-const userName = document.querySelector("#userName")
-const form = document.querySelector("#myForm");
-const markX = document.querySelector("#markX");
-const markO = document.querySelector("#markO");
-let userMark = "";
-let pcMark = "";
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let nameValue = "";
-    if (userName.value == "") {
-        alert("Please Enter A Name");
-    }
-    else {
-        nameValue = userName.value;
-        const userPlayer = Player(nameValue, userMark);
-        const pcPlayer = Player("Computer", pcMark);
-        userPlayer.sayHello();
-        pcPlayer.sayHello();
-        if (markX.checked) {
-            userMark = "X"
-            pcMark = "O"
-            form.style.display = "none";
-            board.gameCont.style.display = "grid"
-            return userMark;
-        }
-        else if (markO.checked) {
-            userMark = "O";
-            pcMark = "X";
-            form.style.display = "none";
-            board.gameCont.style.display = "grid"
-            return userMark;
+
+const Player = (name, mark) => {
+    const playerMark = mark
+    return { name, playerMark };
+}
+
+
+
+
+
+
+
+const gameLogic = (() => {
+    let currentPlayer;
+    const userName = document.querySelector("#userName")
+    const form = document.querySelector("#myForm");
+    const markX = document.querySelector("#markX");
+    const markO = document.querySelector("#markO");
+    let userMark = "";
+    let pcMark = "";
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let nameValue = "";
+        if (userName.value == "") {
+            alert("Please Enter A Name");
         }
         else {
-            alert("Please Select A Mark");
+            nameValue = userName.value;
+            const userPlayer = Player(nameValue, userMark);
+            const pcPlayer = Player("Computer", pcMark);
+
+            if (markX.checked) {
+                userMark = "X"
+                pcMark = "O"
+                form.style.display = "none";
+                board.gameCont.style.display = "grid"
+                return userMark;
+            }
+            else if (markO.checked) {
+                userMark = "O";
+                pcMark = "X";
+                form.style.display = "none";
+                board.gameCont.style.display = "grid"
+                return userMark;
+            }
+            else {
+                alert("Please Select A Mark");
+            }
         }
-    }
-
-})
-
-const marks = document.querySelectorAll(".marks");
-const cells = document.querySelectorAll(".cells");
-for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener("click", () => {
-        marks[i].textContent = userMark;
-        marks[i].style.display = "unset";
 
     })
-}
 
-// create another object with game logic 
+
+    const marks = document.querySelectorAll(".marks");
+    const cells = document.querySelectorAll(".cells");
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", () => {
+            if (marks[i].textContent == "X" | marks[i].textContent == "O") {
+                alert("space taken!")
+            }
+            else {
+                marks[i].textContent = userMark;
+                marks[i].style.display = "unset";
+                cells[i].style.backgroundColor = "lightgrey";
+            }
+        })
+    }
+})();
 
 
 
